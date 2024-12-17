62fab6f0f418e25dfc235b9cf915b03d6a153fdc
Hoyeong Yoo <crystal23733@gmail.com>
2024-12-18 00:54:21 +0900

    완성

--- main.go
+++ main.go
@@ -1,14 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
+var stdin = bufio.NewReader(os.Stdin)
+
+func InputIntValue() (int, error) {
+	var n int
+	_, err := fmt.Scanln(&n)
+	if err != nil {
+		stdin.ReadString('\n')
+	}
+
+	return n, err
+}
+
 func main() {
 	source := rand.NewSource(time.Now().UnixNano())
-	r := rand.New(source)
+	r := rand.New(source).Intn(100)
 
-	fmt.Println("랜덤숫자:", r.Intn(100))
+	cnt := 1
+	for {
+		fmt.Printf("숫자를 입력하세요:")
+		n, err := InputIntValue()
+		if err != nil {
+			fmt.Println("숫자만 입력하세요.")
+		} else {
+			if n > r {
+				fmt.Println("입력된 값이 더 큽니다.")
+			} else if n < r {
+				fmt.Println("입력된 값이 더 작습니다.")
+			} else {
+				fmt.Println("정답입니다! 시도한 횟수:", cnt)
+				break
+			}
+			cnt++
+		}
+	}
 }
